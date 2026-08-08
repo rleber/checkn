f4b4e5e3a919a3e19c32fb489c3ec8c9ae93a2e4
Richard LeBer <richard.leber@gmail.com>
2026-08-07 22:07:53 -0400

    Initial checkin

---
+++ src/module/main.py
@@ -0,0 +1,8 @@
+# Main source file for #TODO:
+
+def main_cli():
+  print("Hello world from template library: ./src/template/main.py function main_cli")
+
+
+def main_gui():
+  print("Hello world from template library: ./src/template/main.py function main_gui")
