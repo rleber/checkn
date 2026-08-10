6ddc8c05e4f699c718cc7aa7016884f7b7242ff5
Richard LeBer <richard.leber@gmail.com>
2026-08-10 17:55:03 -0400

    Working version of psetup

---
+++ src/module/module.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Main scripts
+
+# TODO Rewrite this
+
+import sys
+
+def main_cli(args = sys.argv[1:]):
+    print("Hello world from src/module/module.py:main_cli")
+
+
+
+def main_gui(args = sys.argv[1:]):
+    print("Hello world from src/module/module.py:main_gui")
+
+
+if __name__ == "__main__":
+    main_cli()
